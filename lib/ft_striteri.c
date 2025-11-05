ebb17a159d0a5070134be2b9cad1257ed946f3e1
LLafforgue <ludovic.lafforgue.c@gmail.com>
2025-11-05 21:08:02 +0100

    correct

--- lib/ft_striteri.c
+++ lib/ft_striteri.c
@@ -10,7 +10,17 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+void	ft_stiteri(char *s, void (*f)(unsigned int, char*))
 {
-	
+	unsigned int	index;
+
+	if (!s || !f)
+		return;
+	index = 0;
+	while (s[index])
+	{
+		f(index, &s[index]);
+		index++;
+	}
 }
+
--- lib/ft_strncmp.c
+++ lib/ft_strncmp.c
@@ -12,23 +12,37 @@
 
 #include <stdlib.h>
 
+// int	ft_strncmp(const char *s1, const char *s2, size_t n)
+// {
+// 	char	*temp;
+
+// 	temp = s2;
+// 	while (*s1 && *s2 && n > 0)
+// 	{
+// 		if (*s1 < *s2)
+// 			return (-1);
+// 		if (*s1 > *s2)
+// 			return (1);
+// 		else
+// 		{
+// 			s1++;
+// 			s2++;
+// 			n--;
+// 		}
+// 	}
+// 	return (0);
+// }
+
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	char	*temp;
-
-	temp = s2;
-	while (*s1 && *s2 && n > 0)
+	if (n == 0)
+		return (0);
+	while (*s1 && *s2 && --n > 0)
 	{
-		if (*s1 < *s2)
-			return (-1);
-		if (*s1 > *s2)
-			return (1);
-		else
-		{
-			s1++;
-			s2++;
-			n--;
-		}
+		if (*(unsigned char *)s1 != *(unsigned char *)s2)
+			return (*(unsigned char *)s1 - *(unsigned char *)s2);
+		s1++;
+		s2++;
 	}
-	return (0);
+	return (*(unsigned char *)s1 - *(unsigned char *)s2);
 }
